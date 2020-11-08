01ceb1e2c9a236c5aaf22924a5dc9ce718df663f
Dennis Gosnell <cdep.illabout@gmail.com>
2020-11-08 15:43:40 +0900

    Fix up various other dependencies.

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
 
   miso = import miso-src { };
 
-  # This is just a short cut for accessing the library of Haskell-related
+  # This is just a shortcut for accessing the library of Haskell-related
   # functions provided by nixpkgs.
   haskell-lib = miso.pkgs.haskell.lib;
 
@@ -15,6 +15,7 @@ let
   # existing packages.
   my-ghcjs-pkg-set-overrides = final: prev: {
 
+    # Our local app.
     app = prev.callCabal2nix "app" ./. {};
 
     # json-to-haskell isn't in the set of Hackage packages that the nixpkgs
@@ -34,15 +35,34 @@ let
     # There are various dependencies that appear to be marked as broken.  Here,
     # I manually mark them as unbroken.  Let's hope that they do actually
     # compile.
-
     aeson-extra = haskell-lib.overrideCabal prev.aeson-extra { broken = false; };
-    hedgehog-fn = haskell-lib.overrideCabal prev.hedgehog-fn { broken = false; };
     time-parsers = haskell-lib.overrideCabal prev.time-parsers { broken = false; };
-    nonempty-containers = haskell-lib.overrideCabal prev.nonempty-containers { broken = false; };
 
-    # QuickCheck's tests don't work on GHCJS, so we disable the tests.
-    # This lets the QuickCheck derivation build fine.
+    # Nonempty-containers depends on the hedgehog-fn package for tests, but it
+    # doesn't have required dependencies.  The easiest fix is just to disable
+    # tests for nonempty-containers.
+    #
+    # Also, haddock generation seems messed up for some reason, so disable that
+    # too.
+    nonempty-containers =
+      haskell-lib.overrideCabal
+        (haskell-lib.dontHaddock (haskell-lib.dontCheck prev.nonempty-containers))
+        { broken = false; };
+
+    # Test suits for various packages don't work when run with GHCJS.  We just
+    # disable these.
     QuickCheck = haskell-lib.dontCheck prev.QuickCheck;
+    tasty-quickcheck = haskell-lib.dontCheck prev.tasty-quickcheck;
+    scientific = haskell-lib.dontCheck prev.scientific;
+    base-compat-batteries = haskell-lib.dontCheck prev.base-compat-batteries;
+
+    # A bunch of things use doctest in their tests, but doctest doesn't appear
+    # to be able to be compiled, so we just disable tests for all these.
+    comonad = haskell-lib.dontCheck prev.comonad;
+    lens = haskell-lib.dontCheck prev.lens;
+    semigroupoids = haskell-lib.dontCheck prev.semigroupoids;
+    http-types = haskell-lib.dontCheck prev.http-types;
+    servant = haskell-lib.dontCheck prev.servant;
   };
 
   # Here we create the actual package set by passing our set of overrides.
